ba297c02ab8af6c3343bcfb73f43aa729a6c04f8
Gagan Vasudev <Gagan.Vasudev@mindtree.com>
2016-09-18 00:46:19 +0530

    configured port from process.env

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/',function(req,res) {
     res.sendFile(__dirname+'/index.html');
 });
 
-var port=3000;
+var port = process.env.PORT || 8080;
 
 var server=app.listen(port,function(req,res){
     console.log("Server up at port : "+port);
